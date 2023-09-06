3495484dd194f467f36fa272b50f787dea000dc2
Ethan Wells <ethanmw2000@gmail.com>
2023-09-05 22:46:18 -0500

    more additions to group_anagrams

--- group_anagrams.go
+++ group_anagrams.go
@@ -1,20 +1,58 @@
 package main
 
+import "fmt"
+
 func groupAnagrams(strs []string) [][]string {
 	newArray := [][]string{}
-	wordMap := make(map[byte]int)
+	wordMaps := []map[byte]int{}
+	seenIndexes := make(map[int]int)
 
-	for i := 0; i < len(strs[0]); i++ {
-		wordMap[strs[0][i]] = 0
+	for i := 0; i < len(strs); i++ {
+		wordMaps = append(wordMaps, map[byte]int{})
+		for j := 0; j < len(strs[i]); j++ {
+			wordMaps[i][strs[i][j]] = 1
+		}
 	}
 
-	newArray = append(newArray, []string{strs[0]})
+	newArraySpot := 0
+	current := 0
+	for len(seenIndexes) != len(strs) {
+		fmt.Printf("seenIndexes %d, strs %d", len(seenIndexes), len(strs))
+		newArray = append(newArray, []string{})
+		newArray[newArraySpot] = append(newArray[newArraySpot], strs[current])
+		seenIndexes[current] = 1
 
-	for len(strs) > 1 {
+		for i := current + 1; i < len(strs); i++ {
+			if seenIndexes[i] == 1 {
+				continue
+			}
 
+			innerCount := 1
+			for key := range wordMaps[i] {
+				if _, ok := wordMaps[current][key]; !ok {
+					break
+				}
+
+				if innerCount == len(wordMaps[i]) {
+					newArray[newArraySpot] = append(newArray[newArraySpot], strs[i])
+					seenIndexes[i] = 1
+				}
+				innerCount++
+			}
+		}
+
+		for seenIndexes[current] == 1 {
+			current++
+		}
+		newArraySpot++
 	}
 
-	return [][]string{}
+	return newArray
 }
 
-func sortAnagrams(strs []string, wordMap map[byte]int, newArray [][]string)
+func main() {
+	anagrams := []string{"eat", "ate", "bat", "nat", "tea", "tan"}
+	groupedAnagrams := groupAnagrams(anagrams)
+
+	fmt.Println(groupedAnagrams)
+}
